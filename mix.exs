6f871c9a4f8a6466d20fc0239158e9df5e0cb0fd
Egor Kislitsyn <egor@kislitsyn.com>
2019-03-28 17:51:41 +0700

    prepare for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule PleromaJobQueue.MixProject do
   def project do
     [
       app: :pleroma_job_queue,
+      name: "Pleroma Job Queue",
+      description: "A lightweight job queue",
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
 
       # Docs
       name: "PleromaJobQueue",
@@ -35,6 +38,13 @@ defmodule PleromaJobQueue.MixProject do
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["AGPLv3"],
+      links: %{"GitLab" => "https://git.pleroma.social/pleroma/pleroma_job_queue"}
     ]
   end
 end
